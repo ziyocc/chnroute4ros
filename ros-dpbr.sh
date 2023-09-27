050c06f658a88b33b8108f7988934a3ddaf802d1
GitHubd3b7c <33088850+GitHubd3b7c@users.noreply.github.com>
2023-09-27 21:01:58 +0800

    change to GeoIP2-CN

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,7 +3,7 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/CN-ip-cidr.txt
 
 {
 echo "/ip firewall address-list"

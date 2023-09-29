29e39a2d159244b7939f6db0aa3ff26e67456f02
GitHubd3b7c <33088850+GitHubd3b7c@users.noreply.github.com>
2023-09-29 22:16:13 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,7 +3,7 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/CN-ip-cidr.txt
+wget --no-check-certificate -c -O CN.txt https://ispip.clang.cn/all_cn_cidr.txt
 
 {
 echo "/ip firewall address-list"

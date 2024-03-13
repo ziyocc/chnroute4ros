109394b62721e73ac9d6ddb0245295d90ed25a0b
ziyocc <15770432+ziyocc@users.noreply.github.com>
2024-03-13 18:28:22 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,7 +3,8 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt
+# wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt
+wget --no-check-certificate -c -O CNIP.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
 
 {
 echo "/ip firewall address-list"
@@ -12,7 +13,7 @@ for net in $(cat CN.txt) ; do
   echo "add list=CN address=$net comment=AS4809"
 done
 
-} > ../CN.rsc
+} > ../CNIP.rsc
 
 cd ..
 rm -rf ./pbr
